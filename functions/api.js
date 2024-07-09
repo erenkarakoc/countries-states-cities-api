19a63507859026eef34b043c0b83c935580be052
erenkarakoc <erenkarakoc@aol.com>
2024-07-09 23:09:55 +0300

    commit

--- functions/api.js
+++ functions/api.js
@@ -17,8 +17,7 @@ exports.handler = async (event, context) => {
 
   if (event.httpMethod === "GET") {
     try {
-      const dataPath = path.resolve(__dirname, "functions/countries.json") // Adjust path as per Netlify environment
-
+      const dataPath = path.resolve(__dirname, "countries.json")
       const data = JSON.parse(fs.readFileSync(dataPath, "utf8"))
 
       return {
