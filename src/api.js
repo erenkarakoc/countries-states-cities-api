e5e75ea2ba4331515d26138343fae02d76a25067
erenkarakoc <erenkarakoc@aol.com>
2024-07-09 21:56:12 +0300

    netlify.toml file edited

---
+++ src/api.js
@@ -0,0 +1,23 @@
+exports.handler = async (event, context) => {
+  if (event.httpMethod === "GET") {
+    try {
+      const data = require("./countries.json")
+
+      const headers = {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Headers": "Content-Type",
+      }
+
+      return {
+        statusCode: 200,
+        headers,
+        body: JSON.stringify(data),
+      }
+    } catch (error) {
+      return {
+        statusCode: 500,
+        body: JSON.stringify({ error: "Failed to process GET request" }),
+      }
+    }
+  }
+}
