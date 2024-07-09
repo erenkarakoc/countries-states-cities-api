dfd4f051873671bfbe6d1983d219f8e197b3bc76
erenkarakoc <erenkarakoc@aol.com>
2024-07-09 22:47:34 +0300

    commit

--- functions/api.js
+++ functions/api.js
@@ -17,7 +17,7 @@ exports.handler = async (event, context) => {
 
   if (event.httpMethod === "GET") {
     try {
-      const dataPath = path.resolve(__dirname, "countries.json")
+      const dataPath = path.resolve(__dirname, "functions/countries.json")
       console.log("Data Path:", dataPath) // Log the data path
 
       const data = JSON.parse(fs.readFileSync(dataPath, "utf8"))
