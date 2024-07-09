ec0f0f72166f053d97c4338182213d3123fdd12d
erenkarakoc <erenkarakoc@aol.com>
2024-07-09 22:11:20 +0300

    commit

--- public/api.js
+++ public/api.js
@@ -1,4 +1,17 @@
 exports.handler = async (event, context) => {
+  const headers = {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Headers": "Content-Type",
+    "Access-Control-Allow-Methods": "GET",
+  }
+
+  if (event.httpMethod === "OPTIONS") {
+    return {
+      statusCode: 204,
+      headers,
+    }
+  }
+
   if (event.httpMethod === "GET") {
     try {
       const data = require("./countries.json")
@@ -11,8 +24,15 @@ exports.handler = async (event, context) => {
     } catch (error) {
       return {
         statusCode: 500,
+        headers,
         body: JSON.stringify({ error: "Failed to process GET request" }),
       }
     }
+  }
+
+  return {
+    statusCode: 405,
+    headers,
+    body: JSON.stringify({ error: "Method Not Allowed" }),
   }
 }
